ddff3c948cfe73f0d99b3a10ea95c965a3dd44e7
alkoun-web-design <hassam.sajid.dvlpr@gmail.com>
2026-03-16 17:02:23 +0500

    saved page changes

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,23 +1,45 @@
-import {products} from "../lib/products"
-import React from "react"
-import AddToCartButton from "../../../components/AddToCartButton";;
+import {products} from "@/lib/products"
+import H1 from "@/components/H1";
+import ProductDetails from "@/components/ProductDetails";
+import Link from "next/link"
 
-export default async function Page({params}: { params: Promise<{ productId: number }>}) {
+export default async function Page({params}: { params: Promise<{ slug: string }>}) {
 
-    const productId:number = (await params).productId
-
-    document.title = `Shopping Website | ${products[productId].name} | ${products[productId].description}`
+    const slug = (await params).slug
+    const productBySlug = products.find(product => product.slug === slug);
     
     return (
-        <div>
-            <h1>Product Details {productId}</h1>
-            <ul>
-                <li>Product ID: {products[productId].id}</li>
-                <li>Nmae: {products[productId].name}</li>
-                <li>Price: {products[productId].price}</li>
-                <li>Description: {products[productId].description}</li>
-            </ul>
-            <AddToCartButton product={products[productId]}/>
+        <div className="col-span-full grid grid-cols-subgrid">
+            <div className="col-start-2 col-end-12 mt-20">
+                <Link href="/" className="text-xl text-gray-500 hover:text-gray-900 hover:underline mx-1">Home</Link>
+                <span className="inline-block text-xl  mx-1">/</span>
+                <Link href="/categories" className="text-xl text-gray-500 hover:text-gray-900 hover:underline mx-1">Categories</Link>
+                <span className="inline-block text-xl  mx-1">/</span>
+                { productBySlug ? (
+                    <>
+                        <Link href={`/categories/${productBySlug.category.toLowerCase()}`} className="text-xl text-gray-500 hover:text-gray-900 hover:underline mx-1">{productBySlug.category}</Link>
+                        <span className="inline-block text-xl  mx-1">/</span>
+                        <p className="inline-block text-xl mx-1">{productBySlug.name}</p>
+                    </>
+                ) : (
+                    <p className="inline-block text-xl mx-1">Product Not Found</p>
+                )}
+            </div>
+            { productBySlug ? (
+                <>
+                    <div className="col-span-full justify-self-center">
+                        <H1 heading={productBySlug?.name} />
+                    </div>
+                    <ProductDetails product={productBySlug} />
+                </>
+            ) : (
+                <>
+                    <div className="col-span-full justify-self-center">
+                        <H1 heading="It looks like we don't have that one." />
+                    </div>
+                </>
+            )}
+
         </div>
     )
 }
